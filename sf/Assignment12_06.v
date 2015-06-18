11ca9bad7da690fdece6ea98ed15834c3b03e610
yujunkim <hi@yujun.kim>
2015-06-18 15:05:59 +0900

    delay submission...

--- sf/Assignment12_06.v
+++ sf/Assignment12_06.v
@@ -1,12 +1,37 @@
 Require Export Assignment12_05.
 
 (* problem #06: 10 points *)
+Theorem MoreStlc_value_is_nf: forall v t,
+  value v -> v ==> t -> False.
+Proof with eauto.
+  intros; generalize dependent t; assert (normal_form step v)...
+  unfold normal_form; intros; induction H; intros contra; destruct contra;
+  try solve by inversion; try inversion H0; try inversion H1; subst...
+Qed.
+
+Hint Resolve MoreStlc_value_is_nf.
+
+Theorem MoreStlc_deterministic:
+  deterministic step.
+Proof with eauto.
+  unfold deterministic; intros; generalize dependent y2;
+  induction H as [| |? ? ? ? P| | | | | |? ? ? ? P| | | | | |? ? ? ? P| |? ? ? P|
+  |? ? ? P| | | | | | | | |? ? ? ? P| | | ? ? ? ? ? ? P| |? ? ? P]; intros;
+  inversion H0; subst; try solve by inversion;
+  try replace t0'0 with t0';
+  try replace t1'0 with t1';
+  try replace t2'0 with t2';
+  eauto; exfalso...
+Qed.
+Hint Resolve MoreStlc_deterministic.
 
 Theorem tloop_diverges:
   ~ exists t, tapp tloop (tnat 0) ==>* t /\ normal_form step t.
 Proof with eauto.
+  unfold tloop.
   intros not. inversion not. inversion H.
   unfold normal_form in H1.
+
 
   apply H1.
   inversion H0. subst. subst. eapply ex_intro. unfold tloop. eauto.
@@ -15,13 +40,17 @@ Proof with eauto.
   inversion H4; try solve by inversion. subst.
   inversion H13; try solve by inversion.
   subst.
+  simpl in H5.
+  simpl in H4.
   inversion H5. simpl. eauto 12.
   inversion H6; try solve by inversion. subst.
+  simpl in H6.
+  simpl in H7.
 
-  inversion H7. simpl. eauto 12.
-  inversion H9. try solve by inversion 3.  try solve by inversion. subst.
-
-  remember (tapp tloop (tnat 0)) as p.
+  remember (tapp
+      (tfix
+         (tabs Loop (TArrow TNat TNat)
+            (tabs X TNat (tapp (tvar Loop) (tvar X))))) (tnat 0)) as p.
   remember (tapp
        (tapp
           (tabs Loop (TArrow TNat TNat)
@@ -30,24 +59,23 @@ Proof with eauto.
              (tabs Loop (TArrow TNat TNat)
                 (tabs X TNat (tapp (tvar Loop) (tvar X)))))) (tnat 0)) as q.
   remember (tapp
-       ([Loop
-        := tfix
-             (tabs Loop (TArrow TNat TNat)
-                (tabs X TNat (tapp (tvar Loop) (tvar X))))]
-        tabs X TNat (tapp (tvar Loop) (tvar X))) (tnat 0)) as r.
-
-  remember ([X := tnat 0]
-     tapp
-       (tfix
-          (tabs Loop (TArrow TNat TNat)
-             (tabs X TNat (tapp (tvar Loop) (tvar X))))) (tvar X)) as s.
-  assert (x = p \/ x = q \/ x = r \/ x = s).
-  admit.
-
-  inversion H12. rewrite H15. eauto.
-  inversion H15. rewrite H16. eauto.
-  inversion H16. rewrite H17. eauto. rewrite H17. eauto.
-
+       (tabs X TNat
+          (tapp
+             (tfix
+                (tabs Loop (TArrow TNat TNat)
+                   (tabs X TNat (tapp (tvar Loop) (tvar X))))) (tvar X)))
+       (tnat 0)) as r.
+  assert (forall t k, t = p \/ t = q \/ t = r ->
+    t ==>* k -> k = p \/ k = q\/ k = r).
+  {
+    intros.
+    induction H11. eauto. apply IHmulti. inversion H9. rewrite H15 in H11. eauto.
+    inversion H15. rewrite H16 in H11. eauto. rewrite H16 in H11. eauto.
+  }
+  assert (x = p \/ x = q \/ x = r).
+  apply H9 with (t:=p).
+  eauto. eauto.
+  inversion H11. subst x. eauto. inversion H12. subst x. eauto. subst x. eauto.
   try solve by inversion 3.
 Qed.
 
@@ -55,11 +83,3 @@ Qed.
 Check tloop_diverges:
   ~ exists t, tapp tloop (tnat 0) ==>* t /\ normal_form step t.
 
-Require Export Assignment12_05.
-
-(* problem #06: 10 points *)
-
-Theorem tloop_diverges:
-  ~ exists t, tapp tloop (tnat 0) ==>* t /\ normal_form step t.
-Proof with eauto.
-  intros not. inversion not. inversion H.
